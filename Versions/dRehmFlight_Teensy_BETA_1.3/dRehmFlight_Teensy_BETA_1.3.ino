2fc8200de7c5f46cac41d791b0c73f407e39be9b
Omar N <onadeem157@gmail.com>
2023-03-20 12:32:33 -0400

    Working motor code

--- Versions/dRehmFlight_Teensy_BETA_1.3/dRehmFlight_Teensy_BETA_1.3.ino
+++ Versions/dRehmFlight_Teensy_BETA_1.3/dRehmFlight_Teensy_BETA_1.3.ino
@@ -395,7 +395,7 @@ void loop() {
   loopBlink(); //Indicate we are in main loop with short blink every 1.5 seconds
 
   //Print data at 100hz (uncomment one at a time for troubleshooting) - SELECT ONE:
-  printRadioData();     //Prints radio pwm values (expected: 1000 to 2000)
+  //printRadioData();     //Prints radio pwm values (expected: 1000 to 2000)
   //printDesiredState();  //Prints desired vehicle state commanded in either degrees or deg/sec (expected: +/- maxAXIS for roll, pitch, yaw; 0 to 1 for throttle)
   //printGyroData();      //Prints filtered gyro data direct from IMU (expected: ~ -250 to 250, 0 at rest)
   //printAccelData();     //Prints filtered accelerometer data direct from IMU (expected: ~ -2 to 2; x,y 0 when level, z 1 when level)
@@ -469,13 +469,13 @@ void controlMixer() {
    */
    
   // VTOL Mixing
-  //m1_command_scaled = thro_des + tilt_passthru*roll_PID - (1-tilt_passthru)*yaw_PID; // + (1-tilt_passthru)*roll_PID*0.2;
-  //m2_command_scaled = thro_des - tilt_passthru*roll_PID + (1-tilt_passthru)*yaw_PID; // - (1-tilt_passthru)*roll_PID*0.2:
+  s7_command_scaled = thro_des + tilt_passthru*roll_PID - (1-tilt_passthru)*yaw_PID; // + (1-tilt_passthru)*roll_PID*0.2;
+  s6_command_scaled = thro_des - tilt_passthru*roll_PID + (1-tilt_passthru)*yaw_PID; // - (1-tilt_passthru)*roll_PID*0.2:
 
   //0.5 is centered servo, 0.0 is zero throttle if connecting to ESC for conventional PWM, 1.0 is max throttle
-  //s1_command_scaled = (tilt_passthru*0.5) + 0.33 - tilt_passthru*pitch_PID + tilt_passthru*yaw_PID + (1-tilt_passthru)*roll_PID*4;
-  //s2_command_scaled = 1-(tilt_passthru*0.5) + 0.25 - tilt_passthru*pitch_PID - tilt_passthru*yaw_PID + (1-tilt_passthru)*roll_PID*4;
-  //s3_command_scaled = 0.37 + 3*pitch_PID;
+  s1_command_scaled = (tilt_passthru*0.5) + 0.33 - tilt_passthru*pitch_PID + tilt_passthru*yaw_PID + (1-tilt_passthru)*roll_PID*4;
+  s2_command_scaled = 1-(tilt_passthru*0.5) + 0.25 - tilt_passthru*pitch_PID - tilt_passthru*yaw_PID + (1-tilt_passthru)*roll_PID*4;
+  s3_command_scaled = 0.37 + 3*pitch_PID;
 
  
 }
